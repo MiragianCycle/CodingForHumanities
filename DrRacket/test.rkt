a0bb59ad18c67386117f31deb2d5d024a28a45da
Theena Kumaragurunathan <58807314+MiragianCycle@users.noreply.github.com>
2024-04-23 00:57:17 +0530

    Lua for Writers - Day 1
    Hello WOrld

---
+++ DrRacket/test.rkt
@@ -0,0 +1,15 @@
+; Working with numbers
+(+ 5 3)    ; => 8
+(- 10 4)   ; => 6
+(* 2 6)    ; => 12
+(/ 15 3)   ; => 5
+
+; Working with strings
+(string-append "Hello, " "Lisp!")   ; => "Hello, Lisp!"
+
+; Working with symbols
+(define my-symbol 'symbol-value)
+my-symbol  ; => symbol-value
+
+; Working with lists
+(list 1 2 3 "four" 'five)  ; => '(1 2 3 "four" five)
